870baf45c00e411e8e54c208fff2989b668dc7b1
Oscar Aldrey <oscar.aldrey@rai.usc.es>
2025-12-28 13:43:24 +0100

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import Card from "../../components/Card/Card";
+
+export default function Login ({  }) {
+
+    return (
+        <Card title={"Inicio de sesión"}>
+
+        </Card>
+    )
+    
+}
